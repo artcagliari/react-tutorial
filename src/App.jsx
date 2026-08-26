9f66d240a673ae3e800b576be7bda703c1b7ea01
Artur Cagliari <120335453+artcagliari@users.noreply.github.com>
2026-08-26 11:25:57 -0300

    feat: add theme context and toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
-import Paragrafo from './Paragrafo'
-import CaixaTexto from './assets/CaixaTexto'
+import './App.css';
+import ThemeToggleButton from './components/ThemeToggleButton';
 
+import { ThemeProvider } from './contexts/ThemeContext';
+  
 function App() {
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>COMECE?</h1>
-        </div>
-        <CaixaTexto />
-        <Paragrafo nome="Fulano" />
-        <Paragrafo nome="Beltrano" />
-      </section>
-    </>
-  )
+    <ThemeProvider>
+      <div>
+       <nav>Menu de navegação</nav>
+        <ThemeToggleButton />
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/ThemeToggleButton.jsx
@@ -0,0 +1,14 @@
+import React, {useContext} from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+const ThemeToggleButton = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <button onClick={toggleTheme}>
+      {theme === "light" ? "🌙 Dark Mode" : "☀️ Light Mode"}
+    </button>
+  );
+}
+
+export default ThemeToggleButton;
